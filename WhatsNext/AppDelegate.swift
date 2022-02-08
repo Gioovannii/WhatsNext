bc018ef424af037209b73ea90eb58ff55c3a354e
Giovanni Gaffe <gaffejonathan@ymail.com>
2022-02-08 09:44:05 +0800

    set method to show popover

--- WhatsNext/AppDelegate.swift
+++ WhatsNext/AppDelegate.swift
@@ -5,9 +5,17 @@
 //  Created by Giovanni Gaffé on 2022/2/8.
 //
 
-import Foundation
-import AppKit
+import SwiftUI
 
 class appDelegate: NSObject, NSApplicationDelegate {
+    var statusItem: NSStatusItem?
+    var popover = NSPopover()
+    
+    func showPopover() {
+        guard let statusBarItem = statusItem?.button else { return }
+        
+        popover.show(relativeTo: statusBarItem.bounds, of: statusBarItem, preferredEdge: .maxY)
+    }
+    
     
 }

ee352e70e334ec17ff0199443bccba9e14db7d9c
Giovanni Gaffe <gaffejonathan@ymail.com>
2022-02-08 09:35:52 +0800

    Initial Commit

---
+++ WhatsNext/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WhatsNext
+//
+//  Created by Giovanni Gaffé on 2022/2/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WhatsNext/WhatsNextApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsNextApp.swift
+//  WhatsNext
+//
+//  Created by Giovanni Gaffé on 2022/2/8.
+//
+
+import SwiftUI
+
+@main
+struct WhatsNextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

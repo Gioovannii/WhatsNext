f12fcabc2cfcddcba511d79a4574cc5c609fed49
Giovanni Gaffe <gaffejonathan@ymail.com>
2022-02-08 09:38:13 +0800

    start project

---
+++ WhatsNext/AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  WhatsNext
+//
+//  Created by Giovanni Gaffé on 2022/2/8.
+//
+
+import Foundation
+import AppKit
+
+class appDelegate: NSObject, NSApplicationDelegate {
+    
+}

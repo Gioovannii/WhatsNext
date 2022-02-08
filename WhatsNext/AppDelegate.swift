859daf7a171f8f04395ac5a2a0cd8407a3574a9b
Giovanni Gaffe <gaffejonathan@ymail.com>
2022-02-08 09:53:25 +0800

    set some method to do some work when launch

--- WhatsNext/AppDelegate.swift
+++ WhatsNext/AppDelegate.swift
@@ -7,15 +7,31 @@
 
 import SwiftUI
 
-class appDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate {
     var statusItem: NSStatusItem?
     var popover = NSPopover()
     
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        statusItem?.button?.title = "What's next?"
+        statusItem?.button?.target = self
+        statusItem?.button?.action = #selector(tooglePopover)
+    }
+    
     func showPopover() {
         guard let statusBarItem = statusItem?.button else { return }
-        
         popover.show(relativeTo: statusBarItem.bounds, of: statusBarItem, preferredEdge: .maxY)
     }
     
+    func hidePopover(_ sender: Any) {
+        popover.performClose(sender)
+    }
     
+    @objc func tooglePopover(_ sender: Any) {
+        if popover.isShown {
+            hidePopover(sender)
+        } else {
+            showPopover()
+        }
+    }
 }
